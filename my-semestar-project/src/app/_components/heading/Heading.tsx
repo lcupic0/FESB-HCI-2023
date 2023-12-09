e67fb2a3a79d8a043da28e642bcbbd063e41f476
lcupic0 <lcupic123@gmail.com>
2023-12-09 11:07:46 +0100

    Class Project 6 - Almost Done - pripravak

--- my-semestar-project/src/app/_components/heading/Heading.tsx
+++ my-semestar-project/src/app/_components/heading/Heading.tsx
@@ -9,8 +9,7 @@ export default function Heading(){
             <h1 className={heading.title}>NAŠ KUTAK</h1>
             <p className={heading.desc}>Tajanstveni i šarmantni kutak za prave zaljubljenike u vino i gurmanske delicije. </p>
             <p className={heading.desc}>
-            Ovaj izvanredni ugostiteljski objekt smješten je u idiličnom gradiću Vinogradni Grad, gdje se susreću vinska kultura i kulinarska stručnost. S autentičnom atmosferom i impresivnom kartom vina, "Naš kutak" poziva vas na putovanje koje će zadovoljiti i najizbirljivija nepca.
-              {/* Swiper -> Desno vodi na VINOGRAD npr., pa sljedeca -> na PRŠUT, --> Petrovo polje */}
+              Ovaj izvanredni ugostiteljski objekt smješten je u idiličnom gradiću Vinogradni Grad, gdje se susreću vinska kultura i kulinarska stručnost. S autentičnom atmosferom i impresivnom kartom vina, Naš kutak poziva vas na putovanje koje će zadovoljiti i najizbirljivija nepca.
             </p>
             <FontAwesomeIcon icon={faAnglesDown} className={heading["arrow-down"]}></FontAwesomeIcon>
             <button className={`${heading.button} is-hidden`}>VIŠE</button>
