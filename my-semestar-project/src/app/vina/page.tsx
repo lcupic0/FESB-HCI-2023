db1a5dbceb7d900dbcefaaf24b59a0f1a9642994
lcupic0 <lcupic123@gmail.com>
2023-12-17 16:33:22 +0100

    Defined style for page '/vina' - no functionality

--- my-semestar-project/src/app/vina/page.tsx
+++ my-semestar-project/src/app/vina/page.tsx
@@ -3,7 +3,56 @@ import style from "./vina.module.css"
 export default function Vina() {
     return(
         <div className={style.vina}>
-            Vina Page
+            <div className={style.leftcol}>
+                <div className={style.odabir}>
+                    <p>Kvaliteta:</p>
+                    <form className={style.forma}>
+                        <label className={style.label}>
+                            <input type="radio" className={style.input}/>
+                            Sve
+                        </label>
+                        <label className={style.label}>
+                            <input type="radio" className={style.input}/>
+                            Kvalitetno
+                        </label>
+                        <label className={style.label}>
+                            <input type="radio" className={style.input}/>
+                            Vrhunsko
+                        </label>
+                    </form>
+                </div>
+                <div className={style.odabir}>
+                    <p>Vrsta:</p>
+                    <form className={style.forma}>
+                        <label className={style.label}>
+                            <input type="radio" className={style.input}/>
+                            Crno
+                        </label>
+                        <label className={style.label}>
+                            <input type="radio" className={style.input}/>
+                            Bijelo
+                        </label>
+                        <label className={style.label}>
+                            <input type="radio" className={style.input}/>
+                            Rose
+                        </label>
+                    </form>
+                </div>
+            </div>
+            <div className={style.rightcol}>
+                <div className={style.card}>
+                    Medo
+                </div>
+                <div className={style.card}>
+                    Brundo
+                </div>
+                <div className={style.card}>
+                    Ludo
+                </div>
+                <div className={style.card}>
+                    Zujo
+                </div>
+            </div>
         </div>
     )
 }
